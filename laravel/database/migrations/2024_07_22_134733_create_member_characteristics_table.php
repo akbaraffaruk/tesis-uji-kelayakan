78b9aaf5da8ddc671484c841148ad6aaba22ae76
akbaraffaruk <akbaraffaruk13@gmail.com>
2024-07-22 21:17:43 +0700

    update

---
+++ laravel/database/migrations/2024_07_22_134733_create_member_characteristics_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('member_characteristics', function (Blueprint $table) {
+            $table->id();
+            $table->string('member_number')->unique();
+            $table->string('name');
+            $table->string('identity_number', 16)->unique();
+            $table->string('mother_name')->nullable();
+            $table->string('father_name')->nullable();
+            $table->char('gender', 1)->nullable();
+            $table->string('place_of_birth')->nullable();
+            $table->string('work')->nullable();
+            $table->string('work_description')->nullable();
+            $table->string('source_of_income')->nullable();
+            $table->string('address')->nullable();
+            $table->integer('rice_consumption')->nullable();
+            $table->integer('rice_price')->nullable();
+            $table->integer('kitchen_shop')->nullable();
+            $table->integer('electricity_bills')->nullable();
+            $table->integer('education')->nullable();
+            $table->integer('other_costs')->nullable();
+            $table->integer('savings')->nullable();
+            $table->string('home_status')->nullable();
+            $table->integer('yard_area')->nullable();
+            $table->string('roof_type')->nullable();
+            $table->string('house_wall')->nullable();
+            $table->string('type_of_lighting')->nullable();
+            $table->string('latrine_type')->nullable();
+            $table->string('source_of_drinking_water')->nullable();
+            $table->integer('total_household_income')->nullable();
+            $table->integer('total_household_members')->nullable();
+            $table->integer('income_per_capita')->nullable();
+            $table->integer('tenor')->nullable();
+            $table->integer('pokok')->nullable();
+            $table->integer('margin')->nullable();
+            $table->string('martial_status')->nullable();
+            $table->integer('installment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('member_characteristics');
+    }
+};
